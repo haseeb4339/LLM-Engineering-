ea94b332ed5ef511f476c01c95c29aecac9c515c
haseebblue <haseebullahbj@gmail.com>
2025-05-14 06:15:41 +0500

    i want to make a web summerize for which i use llama as locally i have created web scraper funtion for scrapping the content, system_prompt, and a user prompt funtion

--- implement_ollama_as_api_locally.py
+++ implement_ollama_as_api_locally.py
@@ -20,9 +20,9 @@ Payload = {
     "stream":False
 }
 
-response = requests.post(OLLAMA_API, json=Payload, headers=HEADERS)
+# response = requests.post(OLLAMA_API, json=Payload, headers=HEADERS)
 
-print(response.json()['message']['content'])
+# print(response.json()['message']['content'])
 
 
 ##### we can also use ollama module #####
@@ -33,3 +33,56 @@ print(response.json()['message']['content'])
 
 # print(f'response from ollam: {response['message']['content']}')
 
+
+
+
+def web_scraper(url):
+
+   
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    title = soup.title.string if soup.title else "No title found"
+
+    for irrelevent in soup.body(['script', 'style', 'img', 'input']):
+        irrelevent.decompose()
+
+    text =  soup.body.get_text(separator="\n", strip=True)
+
+    return title, text
+
+
+web=web_scraper("https://edwarddonner.com")
+
+
+# print(web[1])
+
+    
+
+
+
+
+
+system_prompt = "You are an assistant that analyzes the contents of a website\
+    and provides a short summery, ignoring test that might be navigation related\
+    Respond in markdown."
+
+
+
+def user_prompt_for(web):
+    user_prompt = f"\nYou are looking at a website titled {web.title}"
+    user_prompt += "The contents of this website is as follows:\
+        please provide a short summary of this website in markdown.\
+        If it include news or announcements, then summarize these too.\n\n"
+    
+    user_prompt +=web.text
+    
+    return user_prompt
+
+
+##### i will pass web[1]
+def messages_for(web):
+
+    return [
+        {'role':'system', 'content':system_prompt},
+        {'role':'user', 'content':user_prompt_for(web)}
+    ]
